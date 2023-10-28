6a89696ce8443afbcf2bb900c06ccbcf30c067a9
neeraj-gs <gsneeraj05@gmail.com>
2023-10-28 11:34:16 +0530

    ADded commnets on the server initlaisatino skelton , ned to write typeDefs and resolvers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {ApolloServer} = require('@apollo/server')
-const {expressMiddleware} = require('@apollo/server/express4')
+const {ApolloServer} = require('@apollo/server') //apoloo is a library used ot create graphql servers in NOdejs
+const {expressMiddleware} = require('@apollo/server/express4') //integrates apollo server with express
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-async function startServer(){
+async function startServer(){ // set up and start the Express server with Apollo Server integrated.
    const app = express();
-   const server = new ApolloServer({});
+   const server = new ApolloServer({}); //creates a new instance of apollo server,inside confid has typeDefs and resolvers
 
    app.use(bodyParser.json())
    app.use(cors());
